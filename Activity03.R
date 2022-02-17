ad5591940dee0abc3fd126d75791e1fb1a31496f
kroppheather <kroppheather@gmail.com>
2022-02-17 15:54:49 +0000

    read in data

---
+++ Activity03.R
@@ -0,0 +1,6 @@
+#install.packages(c("dplyr","ggplot2"))
+library(dplyr)
+library(ggplot2)
+
+datCO2 <- read.csv("/cloud/project/activity03/annual-co-emissions-by-region.csv")
+datTemp <- read.csv("/cloud/project/activity03/climate-change.csv")
